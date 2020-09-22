d188536800d6a3bb69fc0bac3b53582b834e72bc
flatberg <arnar.flatberg@ntnu.no>
2020-09-22 13:46:23 +0200

    qiime2 2020.8

--- qiime2/install_qiime2R.sh
+++ qiime2/install_qiime2R.sh
@@ -1,4 +1,6 @@
-mamba install -y --force-reinstall biom-format r-remotes bioconductor-rhdf5 bioconductor-biomformat bioconductor-phyloseq r-jsonlite r-yaml bioconductor-rhdf5 bioconductor-rhdf5lib r-tidyverse r-hmisc r-ade4 r-shiny r-nnet r-markdown r-vctrs r-zeallot
+mamba install -y --force-reinstall biom-format r-remotes bioconductor-rhdf5 bioconductor-biomformat bioconductor-phyloseq r-jsonlite r-yaml bioconductor-rhdf5 bioconductor-rhdf5lib r-tidyverse r-hmisc r-ade4 r-shiny r-nnet r-markdown r-vctrs r-zeallot r-data.table
 mamba clean --all
 
-R --slave -e "options(unzip='internal'); Sys.setenv(TAR='/bin/tar'); remotes::install_github('jbisanz/qiime2R')"
+R --slave -e "options(unzip='internal'); Sys.setenv(TAR='/bin/tar'); "
+
+#remotes::install_github('jbisanz/qiime2R')"
